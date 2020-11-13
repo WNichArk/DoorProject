7ca113f061a9fe24f520393810cf281c3609feea
WNichArk <william.nicholson.dev@gmail.com>
2020-11-13 11:26:05 -0600

    Test push

--- src/main/java/co/willnicholson/messaging/JMSService.java
+++ src/main/java/co/willnicholson/messaging/JMSService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class JMSService {
 
+    @JmsListener(destination = "wirelessmsgs")
+    public void listen(String message){
 
+    }
 
 }
